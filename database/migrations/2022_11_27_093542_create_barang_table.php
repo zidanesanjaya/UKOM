a54dc0dc284b5ac95d03db47398f38f4e28720c1
Zidane071 <zidanesanjaya81@Gmail.com>
2022-11-27 17:06:11 +0700

    z

--- database/migrations/2022_11_27_093542_create_barang_table.php
+++ database/migrations/2022_11_27_093542_create_barang_table.php
@@ -13,10 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('pelanggans', function (Blueprint $table) {
+        Schema::create('barang', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('antri');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('pelanggans');
+        Schema::dropIfExists('barang');
     }
 };
--- database/migrations/2022_11_27_093552_create_transaksi_table.php
+++ database/migrations/2022_11_27_093552_create_transaksi_table.php
@@ -13,10 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('transaksi', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->text('harga');
+            $table->string('prize');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('transaksi');
     }
 };
